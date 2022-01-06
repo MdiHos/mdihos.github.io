1332b7ec1b74e9b574f854ccf3a33bad95396cd4
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-01-06 19:03:01 +0330

    Improve the honor cards
    - Fix the bug with StackOverflow stats using the loading state of GitHub requests
    - Fix the problem with the loading icon making the layout vertical
    - Fix the problem with alignments
    - Remove the spinner component and replace it with an image element
    - Rename "spinner.svg" to "loading.svg"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import {ProjectListComponent} from './project-list/project-list.component';
 import {FavoriteBookListComponent} from './favorite-book-list/favorite-book-list.component';
 import {RecommendedBookListComponent} from './recommended-book-list/recommended-book-list.component';
 import {AutographComponent} from './autograph/autograph.component';
-import { SpinnerComponent } from './spinner/spinner.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
@@ -18,7 +17,6 @@ import { HeaderComponent } from './header/header.component';
     AppComponent,
     HeaderComponent,
     ProfileComponent,
-    SpinnerComponent,
     HonorListComponent,
     AutographComponent,
     ProjectListComponent,
